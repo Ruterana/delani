5a5fc739d2cecf8b889cec3016f0e6670a84712a
Ruterana <ncharlene38@gmail.com>
2019-07-26 21:18:58 +0200

    i modify my html index

--- js/script.js
+++ js/script.js
@@ -1,31 +1,59 @@
-$(document).ready(function(){
-   $(".cons").click(function(){
-$(".clcs").toggle();
-    $(".first-icon").toggle();
-   });
-   $(".cons").click(function(){
-    $(".clcs").toggle();
-        $(".first-icon").toggle();
-       });
-   /****developmennt*/
-   $("img.con").click(function(){
-      $('.cls1').toggle();
-      $(".second-icon").toggle();
-   });
-   $(".second-icon").click(function(){
-    $(".cls1").toggle();
-    $(".second-icon").toggle();
- });
-  $("img.coni").click(function(){
-      $(".cls2").toggle();
- $(".third-icon").toggle();
+//design
 
-   });
-   /*** submit on input*/
-   $(".foorm").submit(function(){
-var inputName=$("#name").val();
-var inputEmail=$("#Email").val();
-var x=$("#text").val();
-alert(inputName+" I receive your message. ");
-   } );
+$(document).ready(function () {
+    $("#we1").click(function () {
+        $(".clcs").toggle();
+        $(".first-icon").toggle();
+    });
+    $("#first-icon").click(function () {
+        $(".clcs").toggle();
+        $(".first-icon").toggle();
+    });
+    //development
+    $("#con").click(function () {
+        $('.cls1').toggle();
+        $(".second-icon").toggle();
+    });
+    $("#second-icon").click(function () {
+        $(".cls1").toggle();
+        $(".second-icon").toggle();
+    });
+    // project management
+    $("#coni").click(function () {
+        $(".cls3").toggle();
+        $(".third-icon").toggle();
+
+    });
+    $(".third-icon").click(function () {
+        $(".cls3").toggle();
+        $(".third-icon").toggle();
+
+    });
+
+    /*** submit on input*/
+    $(".foorm").submit(function (event) {
+        var inputName = $("#name").val();
+        var inputEmail = $("#Email").val();
+        var x = $("#text").val();
+        if (inputName == "" || inputEmail == "" || x == "") {
+            alert("please fill the form");
+        }
+        else {
+            alert(inputName + " I receive your message.thank you!!. ");
+        }
+        event.preventDefault();
+    });
+
+//     $(".mgn").hide();
+//     $("mg1").animate({
+//         opacity: 0.7
+//     });
+//     $(".mgb").hover(function () {
+//         $(this).stop().animate({ opacity: 3 });
+//         $(".mgn").fadeIn();
+//     }, function () {
+//         $(this).stop().animate({ opacity: 1 });
+//         $(".mgn").fadeOut();
+//     }
+// })
 });
