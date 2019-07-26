f319cd8efc615f50f94e37950dfa1436a18e0b10
Ruterana <ncharlene38@gmail.com>
2019-07-26 18:43:50 +0200

    i style in javascript

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+   $("img#cons").click(function(){
+$("#first-icon").toggle();
+   });
+   $("img#con").click(function(){
+$("#second-icon").toggle();
+   });
+   $("img#coni").click(function(){
+$("#third-icon").toggle();
+   });
+});
