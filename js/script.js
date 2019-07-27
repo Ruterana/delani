fd0bb321fb47253e7074e12a73a4aca4496a71c8
Ruterana <ncharlene38@gmail.com>
2019-07-27 18:14:47 +0200

    i add style of hovering

--- js/script.js
+++ js/script.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
         }
         event.preventDefault();
     });
-
+/*opacity and hovering for image*/
   $(".mgn").hide();
  $("#mgb").animate({
         opacity: 0.5
@@ -53,6 +53,89 @@ $(document).ready(function () {
         $(this).stop()
         $(".mgn").fadeOut();
     });
-
+/*opacity and hovering for image2*/
+$(".paragraph2").hide();
+ $("#mgc").animate({
+        opacity: 0.5
+     });
+  $("#mgc").hover(function () {
+         $(this).stop()
+        $(".paragraph2").fadeIn();
+    }, function () {
+        $(this).stop()
+        $(".paragraph2").fadeOut();
+    });
+/*opacity and hovering for image3*/
+$(".paragraph3").hide();
+ $("#mgd").animate({
+        opacity: 0.5
+     });
+  $("#mgd").hover(function () {
+         $(this).stop()
+        $(".paragraph3").fadeIn();
+    }, function () {
+        $(this).stop()
+        $(".paragraph3").fadeOut();
+    });
+ /*opacity and hovering for image4*/
+$(".paragraph4").hide();
+$("#mgE").animate({
+       opacity: 0.5
+    });
+ $("#mgE").hover(function () {
+        $(this).stop()
+       $(".paragraph4").fadeIn();
+   }, function () {
+       $(this).stop()
+       $(".paragraph4").fadeOut();
+   });   
+/*opacity and hovering for image5*/
+$(".paragraph5").hide();
+$("#imgf").animate({
+       opacity: 0.5
+    });
+ $("#imgf").hover(function () {
+        $(this).stop()
+       $(".paragraph5").fadeIn();
+   }, function () {
+       $(this).stop()
+       $(".paragraph5").fadeOut();
+   });
+   /*opacity and hovering for image6*/
+$(".paragraph6").hide();
+$("#imgG").animate({
+       opacity: 0.5
+    });
+ $("#imgG").hover(function () {
+        $(this).stop()
+       $(".paragraph6").fadeIn();
+   }, function () {
+       $(this).stop()
+       $(".paragraph6").fadeOut();
+   }); 
+   /*opacity and hovering for image7*/
+$(".paragraph7").hide();
+$("#imgH").animate({
+       opacity: 0.5
+    });
+ $("#imgH").hover(function () {
+        $(this).stop()
+       $(".paragraph7").fadeIn();
+   }, function () {
+       $(this).stop()
+       $(".paragraph7").fadeOut();
+   }); 
+   /*opacity and hovering for image8*/
+$(".paragraph8").hide();
+$("#imgI").animate({
+       opacity: 0.5
+    });
+ $("#imgI").hover(function () {
+        $(this).stop()
+       $(".paragraph8").fadeIn();
+   }, function () {
+       $(this).stop()
+       $(".paragraph8").fadeOut();
+   });                        
 });
 
